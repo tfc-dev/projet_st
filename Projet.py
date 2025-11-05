d9eb574ae9e4d89f1fba24aa7518f722afb1aedd
tfc-dev <51359897+tfc-dev@users.noreply.github.com>
2025-11-05 10:06:19 +0100

    Refactor and clean up Projet.py code

--- Projet.py
+++ Projet.py
@@ -6,9 +6,6 @@ import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 import statsmodels.api as sm
-
-# from scipy._lib._util import _lazywhere 
-
 from pathlib import Path
 
 import warnings
@@ -385,3 +382,4 @@ if choice == choices[3] :
 	**Par contre, avec le dataset 1, nous avons appris à utiliser les librairies Python Geopandas et Folium pour réaliser la cartographie.** 
     """
 	    )
+
